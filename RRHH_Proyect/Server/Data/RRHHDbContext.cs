bf43d09824f3f514a2bec6cb37c2f9d1bd932aa4
villa <villa@SRSOAR>
2024-02-04 21:01:51 -0400

    Agregar archivos de proyecto.

---
+++ RRHH_Proyect/Server/Data/RRHHDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RRHH_Proyect.Server.Data
+{
+    public class RRHHDbContext : DbContext
+    {
+        public RRHHDbContext(DbContextOptions<RRHHDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
