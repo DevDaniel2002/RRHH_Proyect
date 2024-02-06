9d6128bead3f76c2793ea8ad5aaf66d0d6b05f4f
Willys Acevedo <willys.acevedo@ceo.do>
2024-02-05 22:43:09 -0400

    Adding WorkExperience Backend business login

--- RRHH_Proyect/Server/Data/RRHHDbContext.cs
+++ RRHH_Proyect/Server/Data/RRHHDbContext.cs
@@ -15,6 +15,8 @@ namespace RRHH_Proyect.Server.Data
 
         public DbSet<TrainingTypes> TrainingTypes { get; set; }
         public DbSet<Trainings> Trainings { get; set; }
+        public DbSet<WorkExperience> WorkExperience { get; set; }
+        
 
 
     }
